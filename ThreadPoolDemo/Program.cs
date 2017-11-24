3097765a52e409b9289496bc546d54e6573e2434
GreywindGit <GreywindGit@users.noreply.github.com>
2017-11-24 12:50:04 +0100

    Add threadpool demo

---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "The ");
+            ThreadPool.QueueUserWorkItem(callback, "harder ");
+            ThreadPool.QueueUserWorkItem(callback, "I ");
+            ThreadPool.QueueUserWorkItem(callback, "work ");
+            ThreadPool.QueueUserWorkItem(callback, "the ");
+            ThreadPool.QueueUserWorkItem(callback, "luckier ");
+            ThreadPool.QueueUserWorkItem(callback, "I ");
+            ThreadPool.QueueUserWorkItem(callback, "become ");
+            Console.ReadKey();
+
+        }
+
+        static void ShowMyText(object state)
+        {
+            string stateString = $"State: {(string)state}";
+            string threadString = $"Thread: {Thread.CurrentThread.ManagedThreadId}";
+            Console.WriteLine(stateString + threadString);
+        }
+    }
+}
