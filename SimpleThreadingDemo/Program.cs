6045711d1f34e77ec490be7e1cf670b51e8b7fc0
GreywindGit <GreywindGit@users.noreply.github.com>
2017-11-24 11:57:19 +0100

    Add two working threads

--- SimpleThreadingDemo/Program.cs
+++ SimpleThreadingDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace SimpleThreadingDemo
 {
@@ -10,6 +11,27 @@ namespace SimpleThreadingDemo
     {
         static void Main(string[] args)
         {
+            ThreadStart start = new ThreadStart(Counting);
+            Thread firstThread = new Thread(start);
+            Thread secondThread = new Thread(start);
+
+            firstThread.Start();
+            secondThread.Start();
+
+            firstThread.Join();
+            secondThread.Join();
+
+            Console.ReadKey();
+        }
+
+        static void Counting()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.Write($"Current count: {i}");
+                Console.WriteLine($" - Current thread: {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(10);
+            }
         }
     }
 }
